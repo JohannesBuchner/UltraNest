cleanup; MPI works now; KL based on differences; fixed resuming

mininest/netiter.py
@@ -2,6 +2,7 @@ import numpy as np
 from numpy import log, log1p, exp, logaddexp
 import math
 import operator
+import sys
 
 class TreeNode(object):
 	def __init__(self, value=None, id=None, children=None):
@@ -89,8 +90,6 @@ class BreadthFirstIterator(object):
 			assert len(self.active_nodes) == len(self.active_root_ids)
 			assert len(self.active_nodes) == len(self.active_node_values)
 			assert len(self.active_nodes) == len(self.active_node_ids)
-
-import sys
 
 def print_tree(roots, title='Tree:'):
 	print()
@@ -377,19 +376,3 @@ class MultiCounter(object):
 	def remainder_ratio(self):
 		return np.exp(self.logZremain - self.logZ)
 	
-	def integrate_remainder(self, remaining_values):
-		logvol = self.logVolremaining - np.log(len(remaining_values))
-		h = self.H
-		logz = self.logZ
-		for L in remaining_values:
-			logwt = logvol + L
-			logz_new = np.logaddexp(logz, logwt)
-			h = (exp(logwt - logz_new) * node.value + np.exp(logz - logz_new) * (h + logz) - logz_new)
-			logz = logz_new
-		
-		logzerr = (self.H / len(remaining_nodes))**0.5
-		return logz, logzerr
-
-	#def is_tail_dominant(self, remaining_nodes):
-	#	Lmax = max([n.value for n in remaining_nodes])
-

mininest/viz.py
@@ -14,7 +14,7 @@ import scipy.stats
 import string
 clusteridstrings = ['%d' % i for i in range(10)] + list(string.ascii_uppercase)
 
-def nicelogger(points, info, region, transformLayer):
+def nicelogger(points, info, region, transformLayer, region_fresh=False):
     #u, p, logl = points['u'], points['p'], points['logl']
     p = points['p']
     paramnames = info['paramnames']
@@ -36,7 +36,8 @@ def nicelogger(points, info, region, transformLayer):
     else:
         columns, _rows = 80, 25
 
-    width = columns - 22 - max([len(pname) for pname in paramnames])
+    paramwidth = max([len(pname) for pname in paramnames])
+    width = columns - 23 - paramwidth
     indices = ((p - plo_rounded) * width / (phi_rounded - plo_rounded).reshape((1, -1))).astype(int)
     indices[indices >= width] = width - 1
     indices[indices < 0] = 0
@@ -47,7 +48,7 @@ def nicelogger(points, info, region, transformLayer):
     clusterids = transformLayer.clusterids % len(clusteridstrings)
     nmodes = transformLayer.nclusters
     print("Mono-modal" if nmodes == 1 else "Have %d modes" % nmodes, 
-        "Volume: %.2e" % region.estimate_volume())
+        "Volume: %.2e" % region.estimate_volume(), '*' if region_fresh else ' ')
 
     if ndim == 1:
         pass
@@ -113,7 +114,7 @@ def nicelogger(points, info, region, transformLayer):
                 line = line[:j] + rightstr + line[j + len(rightstr):]
 
 			
-        
-        print('%09s|%s|%9s %s' % (fmt % plo_rounded[i], line, fmt % phi_rounded[i], param))
+        parampadded = ('%-%%ds' % paramwidth) % param
+        print('%s: %09s|%s|%9s' % (parampadded, fmt % plo_rounded[i], line, fmt % phi_rounded[i]))
     
     