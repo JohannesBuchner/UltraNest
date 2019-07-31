eb36d1a41553736545c2a129e12b149ef5f2a435
Johannes Buchner <johannes.buchner.acad@gmx.com>
2019-07-31 10:38:18 +0200

    avoid python list in favor of numpy array (shoud fix copy error)

--- mininest/integrator.py
+++ mininest/integrator.py
@@ -779,7 +779,7 @@ class ReactiveNestedSampler(object):
                 
                 #if self.log:
                 #    self.logger.info('Making region from %d parents at L=%.1f...' % (len(active_nodes), Lmin))
-                active_node_ids = [n.id for n in active_nodes]
+                active_node_ids = np.asarray([n.id for n in active_nodes])
                 active_u = self.pointpile.getu(active_node_ids)
                 self.update_region(
                     active_u=active_u, active_node_ids=active_node_ids)
