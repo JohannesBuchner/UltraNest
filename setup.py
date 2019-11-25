6be006349dd0362ae0d0bdf891527f48b63c9a69
Johannes Buchner <johannes.buchner.acad@gmx.com>
2019-11-25 21:26:31 +0100

    Bump version: 2.0.1 → 2.0.2

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/JohannesBuchner/ultranest',
-    version='2.0.1',
+    version='2.0.2',
     zip_safe=False,
     cmdclass={'build_ext': build_ext},
 )
