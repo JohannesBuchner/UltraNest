3af1027bd02892e55ed8d49b8627f384c5e9d080
Johannes Buchner <johannes.buchner.acad@gmx.com>
2020-12-07 00:53:20 +0100

    [ci] debugging for mpi

--- examples/testfeatures.py
+++ examples/testfeatures.py
@@ -14,18 +14,19 @@ import hashlib
 def get_arg_hash(runargs):
     return hashlib.md5(str(sorted(runargs.items())).encode()).hexdigest()[:10]
 
+
 def main(args):
     ndim = args.x_dim
     paramnames = list(string.ascii_lowercase)[:ndim]
-    
+
     np.random.seed(args.seed)
     if args.wrapped_dims:
         wrapped_params = [True] * ndim
     else:
         wrapped_params = None
-    
+
     true_Z = None
-    
+
     if args.log_dir is None:
         if args.delete_dir:
             return
@@ -35,23 +36,24 @@ def main(args):
         log_dir += '-%dd' % ndim
         if args.wrapped_dims:
             log_dir += '-wrapped'
-    
+
         if args.delete_dir:
             shutil.rmtree(log_dir, ignore_errors=True)
-    
+
     if ndim >= 20 and args.num_live_points >= 1000:
         print("skipping, probably too slow to run")
         return
-    
+
     if args.problem == 'gauss':
         sigma = 0.01
         if args.wrapped_dims:
-            centers = (np.sin(np.arange(ndim)/2.) + 1.) / 2.
+            centers = (np.sin(np.arange(ndim) / 2.) + 1.) / 2.
         else:
-            centers = (np.sin(np.arange(ndim)/2.) / 2. + 1.) / 2.
+            centers = (np.sin(np.arange(ndim) / 2.) / 2. + 1.) / 2.
         true_Z = 0
+
         def loglike(theta):
-            like = -0.5 * (((theta - centers)/sigma)**2).sum(axis=1) - 0.5 * np.log(2 * np.pi * sigma**2) * ndim
+            like = -0.5 * (((theta - centers) / sigma)**2).sum(axis=1) - 0.5 * np.log(2 * np.pi * sigma**2) * ndim
             return like
 
         def transform(x):
@@ -59,7 +61,7 @@ def main(args):
     elif args.problem == 'slantedeggbox':
         if not args.pass_transform:
             return
-        
+
         def loglike(z):
             chi = (2. + (np.cos(z[:,:2] / 2.)).prod(axis=1))**5
             chi2 = -np.abs((z - 5 * np.pi) / 0.5).sum(axis=1)
@@ -68,38 +70,40 @@ def main(args):
         def transform(x):
             return x * 100
     elif args.problem == 'funnel':
-        if args.wrapped_dims: return
+        if args.wrapped_dims:
+            return
         if not args.pass_transform:
             return
-        
+
         sigma = 0.01
         centers = np.sin(np.arange(ndim) / 2.)
         data = np.random.normal(centers, sigma).reshape((1, -1))
 
         def loglike(theta):
             sigma = 10**theta[:,0]
-            like = -0.5 * (((theta[:,1:] - data)/sigma.reshape((-1, 1)))**2).sum(axis=1) - 0.5 * np.log(2 * np.pi * sigma**2) * ndim
+            like = -0.5 * (((theta[:,1:] - data) / sigma.reshape((-1, 1)))**2).sum(axis=1) - 0.5 * np.log(2 * np.pi * sigma**2) * ndim
             return like
 
         def transform(x):
             z = x * 20 - 10
             z[:,0] = x[:,0] * 6 - 3
             return z
-        
+
         paramnames.insert(0, 'sigma')
     elif args.problem == 'loggamma':
         true_Z = 0.0
-        if args.wrapped_dims: return
-        rv1a = scipy.stats.loggamma(1, loc=2./3, scale=1./30)
-        rv1b = scipy.stats.loggamma(1, loc=1./3, scale=1./30)
-        rv2a = scipy.stats.norm(2./3, 1./30)
-        rv2b = scipy.stats.norm(1./3, 1./30)
+        if args.wrapped_dims:
+            return
+        rv1a = scipy.stats.loggamma(1, loc=2. / 3, scale=1. / 30)
+        rv1b = scipy.stats.loggamma(1, loc=1. / 3, scale=1. / 30)
+        rv2a = scipy.stats.norm(2. / 3, 1. / 30)
+        rv2b = scipy.stats.norm(1. / 3, 1. / 30)
         rv_rest = []
         for i in range(2, ndim):
-            if i <= (ndim+2)/2:
-                rv = scipy.stats.loggamma(1, loc=2./3., scale=1./30)
+            if i <= (ndim + 2) / 2:
+                rv = scipy.stats.loggamma(1, loc=2. / 3., scale=1. / 30)
             else:
-                rv = scipy.stats.norm(2./3, 1./30)
+                rv = scipy.stats.norm(2. / 3, 1. / 30)
             rv_rest.append(rv)
             del rv
 
@@ -111,35 +115,40 @@ def main(args):
             like = np.where(like < -300, -300 - ((np.asarray(theta) - 0.5)**2).sum(), like)
             assert like.shape == (len(theta),), (like.shape, theta.shape)
             return like
+
         def transform(x):
             return x
-    
+
     from ultranest import ReactiveNestedSampler
-    sampler = ReactiveNestedSampler(paramnames, loglike, 
-        transform=transform if args.pass_transform else None, 
+    sampler = ReactiveNestedSampler(
+        paramnames, loglike,
+        transform=transform if args.pass_transform else None,
         log_dir=log_dir, vectorized=True,
         resume='resume' if args.resume else 'overwrite',
         wrapped_params=wrapped_params,
     )
+    print("MPI:", sampler.mpi_size, sampler.mpi_rank)
     for result in sampler.run_iter(
-            update_interval_volume_fraction=args.update_interval_iter_fraction,
-            dlogz=args.dlogz,
-            dKL=args.dKL,
-            frac_remain=args.frac_remain,
-            min_ess=args.min_ess,
-            max_iters=args.max_iters,
-            cluster_num_live_points=args.cluster_num_live_points,
-            min_num_live_points=args.num_live_points,
-            max_ncalls=int(args.max_ncalls),
-        ):
+        update_interval_volume_fraction=args.update_interval_iter_fraction,
+        dlogz=args.dlogz,
+        dKL=args.dKL,
+        frac_remain=args.frac_remain,
+        min_ess=args.min_ess,
+        max_iters=args.max_iters,
+        cluster_num_live_points=args.cluster_num_live_points,
+        min_num_live_points=args.num_live_points,
+        max_ncalls=int(args.max_ncalls),
+    ):
         sampler.print_results()
-        print(" (remember, we are trying to achive: %s ) " % (
-            dict(dlogz=args.dlogz,
-                dKL=args.dKL,
-                frac_remain=args.frac_remain,
-                min_ess=args.min_ess,
-            )))
-    
+        print(
+            " (remember, we are trying to achive: %s ) " % (
+                dict(
+                    dlogz=args.dlogz,
+                    dKL=args.dKL,
+                    frac_remain=args.frac_remain,
+                    min_ess=args.min_ess,
+                )))
+
     results = sampler.results
     sampler.plot()
     sampler.pointstore.close()
@@ -147,18 +156,20 @@ def main(args):
         assert results['logz'] - results['logzerr'] * 3 < true_Z < results['logz'] + results['logzerr'] * 3
     return results
 
+
 def run_safely(runargs):
     id = get_arg_hash(runargs)
     if os.path.exists('testfeatures/%s.done' % id):
         return
-    
+
     print("Running %s with options:" % id, runargs)
-    
+
     def timeout_handler(signum, frame):
         raise Exception("Timeout")
+
     signal.signal(signal.SIGALRM, timeout_handler)
-    
-    signal.alarm(60 * (1 + runargs['x_dim'])) # give a few minutes
+
+    signal.alarm(60 * (1 + runargs['x_dim']))  # give a few minutes
     try:
         main(AttrDict(runargs))
     except Exception:
@@ -171,6 +182,7 @@ def run_safely(runargs):
     signal.alarm(0)
     with open('testfeatures/%s.done' % id, 'w'):
         pass
+
 
 class AttrDict(dict):
     def __init__(self, *args, **kwargs):
@@ -189,7 +201,7 @@ if __name__ == '__main__':
     parser.add_argument('conf', nargs='*', help='config files')
 
     progargs = parser.parse_args()
-    
+
     if len(progargs.conf) > 0:
         for filename in progargs.conf:
             print("loading configuration from file '%s'..." % filename)
@@ -199,7 +211,7 @@ if __name__ == '__main__':
             if progargs.timeout:
                 run_safely(runargs)
         sys.exit(0)
-    
+
     if progargs.random:
         def choose(myargs):
             # pick first (default) option most of the time
@@ -210,12 +222,12 @@ if __name__ == '__main__':
     else:
         def choose(myargs):
             return myargs
-        
+
     Nrounds = progargs.nrounds
     i = 0
     while True:
         print("generating a random configuration...")
-        
+
         runargs = dict(
             problem = choose(['gauss', 'slantedeggbox', 'funnel', 'loggamma']),
             x_dim = choose([2, 1, 6, 20]),
@@ -236,7 +248,7 @@ if __name__ == '__main__':
             max_ncalls = choose([10000000., 10000., 100000.]),
         )
         if not progargs.random:
-            key = i 
+            key = i
             nkeys = len(runargs.keys())
             for k, v in runargs.items():
                 if 0 <= key <= len(v):
@@ -250,7 +262,7 @@ if __name__ == '__main__':
             print("Storing configuration as '%s'. Options were:" % filename, runargs)
             with open(filename, 'w') as f:
                 json.dump(runargs, f, indent=2)
-            #run_safely(runargs)
+            # run_safely(runargs)
             if key > 0:
                 break
         else:
