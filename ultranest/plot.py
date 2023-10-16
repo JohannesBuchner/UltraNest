d87653cb8a71b7270fa94457f46c6d6af77bdc7e
facero <fabio.f.acero@gmail.com>
2023-10-16 13:43:03 +0200

    Add the possibility to provide an ax for plots
    In a spectral plot with a residual panel the current `band.line` plots the line on the 2nd residual panel.
    This allows to select the ax where the line or shade will be overlaid.

--- ultranest/plot.py
+++ ultranest/plot.py
@@ -118,22 +118,28 @@ class PredictionBand(object):
         assert len(self.ys) > 0, self.ys
         return scipy.stats.mstats.mquantiles(self.ys, q, axis=0)[0]
 
-    def shade(self, q=0.341, **kwargs):
+   def shade(self, q=0.341, ax=None, **kwargs):
         """Plot a shaded region between 0.5-q and 0.5+q. Default is 1 sigma."""
         if not 0 <= q <= 0.5:
-            raise ValueError("quantile distance from the median, q, must be between 0 and 0.5, not %s. For a 99%% quantile range, use q=0.48." % q)
+            raise ValueError("quantile distance from the median, q, must be between 0 and 0.5, not %s. For a 99% quantile range, use q=0.48." % q)
         shadeargs = dict(self.shadeargs)
         shadeargs.update(kwargs)
         lo = self.get_line(0.5 - q)
         hi = self.get_line(0.5 + q)
-        return plt.fill_between(self.x, lo, hi, **shadeargs)
+        if ax is None:
+            return plt.fill_between(self.x, lo, hi, **shadeargs)
+        else:
+            return ax.fill_between(self.x, lo, hi, **shadeargs)
 
-    def line(self, **kwargs):
+    def line(self, ax= None, **kwargs):
         """Plot the median curve."""
         lineargs = dict(self.lineargs)
         lineargs.update(kwargs)
         mid = self.get_line(0.5)
-        return plt.plot(self.x, mid, **lineargs)
+        if ax is None:
+            return plt.plot(self.x, mid, **lineargs)
+        else:
+            return ax.plot(self.x, mid, **lineargs)
 
 
 # the following function is taken from https://github.com/joshspeagle/dynesty/blob/master/dynesty/plotting.py
