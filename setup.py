4748f00ef208c0f5db55e33a4a995e3e0e648ec7
Johannes Buchner <johannes.buchner.acad@gmx.com>
2024-10-26 20:33:20 +0200

    Bump version: 4.3.2 → 4.3.3

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/JohannesBuchner/ultranest',
-    version='4.3.2',
+    version='4.3.3',
     zip_safe=False,
     cmdclass={'build_ext': build_ext},
 )
